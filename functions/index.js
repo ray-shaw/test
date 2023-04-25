69cdd1acb778287d34e8660942ef7ebcc28a9bfc
ray-shaw <99449623+ray-shaw@users.noreply.github.com>
2023-04-25 12:51:38 +1000

    add functions

---
+++ functions/index.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+const functions = require("firebase-functions");
+
+// // Create and deploy your first functions
+// // https://firebase.google.com/docs/functions/get-started
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
